8449663747a1b380fb0198cbcbe81433e3bf7b8e
musenkishi <musenkishi@gmail.com>
2025-03-05 11:26:47 +0100

    rename to forked WIP name

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ function noSearchDefaultPageRender() {
   app.innerHTML = `
     <div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100vh;">
       <div class="content-container">
-        <h1>Und*ck</h1>
+        <h1>Snabb*</h1>
         <p>DuckDuckGo's bang redirects are too slow. Add the following URL as a custom search engine to your browser. Enables <a href="https://duckduckgo.com/bang.html" target="_blank">all of DuckDuckGo's bangs.</a></p>
         <div class="url-container"> 
           <input 
             type="text" 
             class="url-input"
-            value="https://unduck.link?q=%s"
+            value="https://snabb.musen.dev?q=%s"
             readonly 
           />
           <button class="copy-button">
@@ -21,11 +21,12 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
+        <a href="https://www.musen.dev" target="_blank">musen</a>
         •
-        <a href="https://x.com/theo" target="_blank">theo</a>
+        <a href="https://bsky.app/profile/musen.dev" target="_blank">@musen.dev</a>
         •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/musenkishi/unduck" target="_blank">github</a>
+      <p>*This is a fork of Theo's <a href="https://github.com/t3dotgg/unduck" target="_blank">Und*ck</a>. Instead of defaulting to Google, it defaults to <a href="https://leta.mullvad.net/faq" target="_blank">Mullvad's Leta</a>, an anonymized search proxy.</p>
       </footer>
     </div>
   `;
@@ -44,7 +45,7 @@ function noSearchDefaultPageRender() {
   });
 }
 
-const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "g";
+const LS_DEFAULT_BANG = localStorage.getItem("default-bang") ?? "leta";
 const defaultBang = bangs.find((b) => b.t === LS_DEFAULT_BANG);
 
 function getBangredirectUrl() {
